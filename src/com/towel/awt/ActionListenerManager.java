10255cb7b7084e16bde64d79f0a121c9cde52a17
marcobiscaro2112 <marcobiscaro2112@gmail.com>
2010-12-27 20:46:17 -0200

    Commented files in package com.towel.awt

--- src/com/towel/awt/ActionListenerManager.java
+++ src/com/towel/awt/ActionListenerManager.java
@@ -9,25 +9,65 @@ import java.util.Map;
 
 import javax.swing.AbstractButton;
 
+/**
+ * Manager that maps an {@link ActionListener} to a button.
+ * 
+ * @author Marcos A. Vasconcelos Junior
+ */
 public class ActionListenerManager implements ActionListener {
 	private List<ActionListener> before, after;
 	private Map<AbstractButton, ActionListener> map;
 
+	/**
+	 * Creates a new <code>ActionListenerManager</code>.
+	 */
 	public ActionListenerManager() {
 		map = new HashMap<AbstractButton, ActionListener>();
 		before = new ArrayList<ActionListener>();
 		after = new ArrayList<ActionListener>();
 	}
 
+	/**
+	 * Associates a button with an <code>ActionListener</code>, that will have
+	 * its {@link ActionListener#actionPerformed(ActionEvent) actionPerformed}
+	 * method called when the button has been pressed.
+	 * 
+	 * @param source
+	 *            the button
+	 * @param action
+	 *            the listener that will be called
+	 */
 	public void manage(AbstractButton source, ActionListener action) {
 		map.put(source, action);
 		source.addActionListener(this);
 	}
 
+	/**
+	 * Adds a new <code>ActionListener</code> that will be called when any
+	 * mapped button is pressed. That <code>ActionListener</code>s will be
+	 * called before the specific button's action.
+	 * 
+	 * The <code>ActionListener</code>s registered with this method are called
+	 * even if the specific button <code>ActionListener</code> fails to execute.
+	 * 
+	 * @param action
+	 *            the <code>ActionListener</code> that will be called
+	 */
 	public void doBefore(ActionListener action) {
 		before.add(action);
 	}
 
+	/**
+	 * Adds a new <code>ActionListener</code> that will be called when any
+	 * mapped button is pressed. That <code>ActionListener</code>s will be
+	 * called after the specific button's action.
+	 * 
+	 * The <code>ActionListener</code>s registered with this method are called
+	 * even if the specific button <code>ActionListener</code> fails to execute.
+	 * 
+	 * @param action
+	 *            the <code>ActionListener</code> that will be called
+	 */
 	public void doAfter(ActionListener action) {
 		after.add(action);
 	}
@@ -45,9 +85,26 @@ public class ActionListenerManager implements ActionListener {
 			act.actionPerformed(arg0);
 	}
 
+	/**
+	 * Exception to encapsulate checked exceptions when implementing method
+	 * {@link Action#doAction()}.
+	 * 
+	 * @author Marcos A. Vasconcelos Junior
+	 */
+	@SuppressWarnings("serial")
 	public static class StopException extends RuntimeException {
+		/**
+		 * The real cause of this exception.
+		 */
 		public Exception cause;
 
+		/**
+		 * Creates a new unchecked exception that encapsulates the given
+		 * exception.
+		 * 
+		 * @param cause
+		 *            the real cause
+		 */
 		public StopException(Exception cause) {
 			this.cause = cause;
 		}
--- src/com/towel/awt/ActionManager.java
+++ src/com/towel/awt/ActionManager.java
@@ -9,26 +9,64 @@ import java.util.Map;
 
 import javax.swing.AbstractButton;
 
+/**
+ * Manager that maps an action to a button.
+ * 
+ * @author Marcos A. Vasconcelos Junior
+ * @deprecated Replaced by class {@link com.towel.awt.ann.ActionManager}.
+ */
 @Deprecated
 public class ActionManager implements ActionListener {
 	private List<Action> before, after;
 	private Map<AbstractButton, Action> map;
 
+	/**
+	 * Creates a new <code>ActionManager</code>.
+	 */
 	public ActionManager() {
 		map = new HashMap<AbstractButton, Action>();
 		before = new ArrayList<Action>();
 		after = new ArrayList<Action>();
 	}
 
+	/**
+	 * Associates a button with an action, that will be executed when the button
+	 * has been pressed.
+	 * 
+	 * @param source
+	 *            the button
+	 * @param action
+	 *            the action to execute
+	 */
 	public void manage(AbstractButton source, Action action) {
 		map.put(source, action);
 		source.addActionListener(this);
 	}
 
+	/**
+	 * Adds a new action to be done when any mapped button is pressed. That
+	 * actions will be executed before the specific button's action.
+	 * 
+	 * The actions registered with this method are called even if the specific
+	 * button action fails to execute.
+	 * 
+	 * @param action
+	 *            the action to execute
+	 */
 	public void doBefore(Action action) {
 		before.add(action);
 	}
 
+	/**
+	 * Adds a new action to be done when any mapped button is pressed. That
+	 * actions will be executed after the specific button's action.
+	 * 
+	 * The actions registered with this method are called even if the specific
+	 * button action fails to execute.
+	 * 
+	 * @param action
+	 *            the action to execute
+	 */
 	public void doAfter(Action action) {
 		after.add(action);
 	}
@@ -46,9 +84,26 @@ public class ActionManager implements ActionListener {
 			act.doAction();
 	}
 
-	public static class StopException extends RuntimeException {
+	/**
+	 * Exception to encapsulate checked exceptions when implementing method
+	 * {@link Action#doAction()}.
+	 * 
+	 * @author Marcos A. Vasconcelos Junior
+	 */
+	@SuppressWarnings("serial")
+	public class StopException extends RuntimeException {
+		/**
+		 * The real cause of this exception.
+		 */
 		public Exception cause;
 
+		/**
+		 * Creates a new unchecked exception that encapsulates the given
+		 * exception.
+		 * 
+		 * @param cause
+		 *            the real cause
+		 */
 		public StopException(Exception cause) {
 			this.cause = cause;
 		}
